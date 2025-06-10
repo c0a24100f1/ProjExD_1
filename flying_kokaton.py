b19bd4e17d57bad63dee169d13da0578374e554b
Daichi Sakata <c0a24100f1@edu.teu.ac.jp>
2025-06-10 17:55:07 +0900

    演習３

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -28,7 +28,11 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
+            kk_rct.move_ip(2, 0)
+
+        kk_rct.move_ip(-1, 0)
+
+
         
         x =tmr%3200
         screen.blit(bg_img, [-x, 0]) #1枚目
